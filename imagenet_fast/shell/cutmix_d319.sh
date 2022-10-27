3b4092a75ee3edf49b53f9785492b48bbc8a1a5c
Neonsign247 <haeil247@gmail.com>
2022-10-27 17:33:06 +0900

    133 drop class

--- imagenet_fast/shell/cutmix_d319.sh
+++ imagenet_fast/shell/cutmix_d319.sh
@@ -1,5 +1,5 @@
-GPU=0,1
-NAME=cutmix_d0.0
+GPU=4,5,6,7
+NAME=cutmix_d319
 
 DATA160=/workspace/dataset/ILSVRC2012-sz/160
 DATA352=/workspace/dataset/ILSVRC2012-sz/352
@@ -9,8 +9,8 @@ CONFIG2=configs/${NAME}/configs_fast_phase2.yml
 CONFIG3=configs/${NAME}/configs_fast_phase3.yml
 
 PREFIX1=${NAME}_phase1
-PREFIX2=${NAME}_phase1
-PREFIX3=${NAME}_phase1
+PREFIX2=${NAME}_phase2
+PREFIX3=${NAME}_phase3
 
 OUT1=out/${NAME}_train_phase1.out
 OUT2=out/${NAME}_train_phase2.out
@@ -25,19 +25,19 @@ END2=./trained_models/${PREFIX2}/checkpoint_epoch40.pth.tar
 END3=./trained_models/${PREFIX3}/checkpoint_epoch100.pth.tar
 
 # training for phase 1
-CUDA_VISIBLE_DEVICES=$GPU python -u cutmix_drop.py $DATA160 -c $CONFIG1 --output_prefix $PREFIX1 | tee $OUT1
+CUDA_VISIBLE_DEVICES=$GPU python -u cutmix_drop_class.py $DATA160 -c $CONFIG1 --output_prefix $PREFIX1 | tee $OUT1
 
 # evaluation for phase 1
 # CUDA_VISIBLE_DEVICES=$GPU python -u main_fast.py $DATA352 -c $CONFIG1 --output_prefix $PREFIX1 --resume $END1  --evaluate | tee $EVAL1
 
 # training for phase 2
-CUDA_VISIBLE_DEVICES=$GPU python -u cutmix_drop.py $DATA352 -c $CONFIG2 --output_prefix $PREFIX2 --resume $END1 | tee $OUT2
+CUDA_VISIBLE_DEVICES=$GPU python -u cutmix_drop_class.py $DATA352 -c $CONFIG2 --output_prefix $PREFIX2 --resume $END1 | tee $OUT2
 
 # evaluation for phase 2
 # CUDA_VISIBLE_DEVICES=$GPU python -u main_fast.py $DATA352 -c $CONFIG2 --output_prefix $PREFIX2 --resume $END2 --evaluate | tee $EVAL2
 
 # training for phase 3
-CUDA_VISIBLE_DEVICES=$GPU python -u cutmix_drop.py $DATA352 -c $CONFIG3 --output_prefix $PREFIX3 --resume $END2 | tee $OUT3
+CUDA_VISIBLE_DEVICES=$GPU python -u cutmix_drop_class.py $DATA352 -c $CONFIG3 --output_prefix $PREFIX3 --resume $END2 | tee $OUT3
 
 # evaluation for phase 3
 # CUDA_VISIBLE_DEVICES=$GPU python -u main_fast.py $DATA -c $CONFIG3 --output_prefix $PREFIX3 --resume $END3 --evaluate | tee $EVAL3
