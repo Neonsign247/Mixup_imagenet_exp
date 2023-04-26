ea90392f3d0b8a2bcb8336b23f773795a8c2d511
Neonsign247 <haeil247@gmail.com>
2023-04-27 03:38:42 +0900

    lr 0.2

--- run_cutmix.sh
+++ run_cutmix.sh
@@ -5,9 +5,9 @@ shift
 echo CUDA_VISIBLE_DEVICES=$NUM_PROC 
 export CUDA_VISIBLE_DEVICES=$NUM_PROC
 echo $PROB
-for i in 0 1 2 3 4 5 6 7 8 9
+for i in 0 1 2 3 4
 do
     python main.py --dataset cifar100 --labels_per_class 500 --arch wrn28_2  \
-        --learning_rate 0.1 --momentum 0.9 --decay 0.0001 --epochs 300 \
+        --learning_rate 0.2 --momentum 0.9 --decay 0.0001 --epochs 300 \
         --schedule 100 200 --gammas 0.1 0.1 --train mixup --box True --mixup_alpha 1.0 --mixup_prob $PROB --seed $i
 done
