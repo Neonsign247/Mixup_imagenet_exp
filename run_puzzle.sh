afcc96b6519a483442e9fa9e643b47f4f43ff7e7
Neonsign247 <haeil247@gmail.com>
2023-05-12 23:37:40 +0900

    wrn28_10

--- run_puzzle.sh
+++ run_puzzle.sh
@@ -7,7 +7,7 @@ export CUDA_VISIBLE_DEVICES=$NUM_PROC
 echo $PROB
 for i in 0 1 2 3 4
 do
-    python main.py --dataset cifar100 --labels_per_class 500 --arch resnet56 \
+    python main.py --dataset cifar100 --labels_per_class 500 --arch wrn28_10 \
         --learning_rate 0.2 --momentum 0.9 --decay 0.0001 --epochs 300 \
         --schedule 100 200 --gammas 0.1 0.1 --train mixup --mixup_alpha 1.0 \
         --graph True --n_labels 3 --eta 0.2 --beta 1.2 --gamma 0.5 --neigh_size 4 --transport True --t_size 4 --t_eps 0.8 \
